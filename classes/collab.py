031afdc1068aad001046a41cfcb484b1ebe2d4c7
confusedDip <anihero4@gmail.com>
2024-04-26 15:29:54 -0700

    Added Share and Unshare

--- classes/collab.py
+++ classes/collab.py
@@ -1,28 +1,36 @@
+import datetime
 from collections import deque
 from itertools import combinations
 import pygraphviz as pgv
+
 
 class Context:
 
     def __init__(self, users):
         self.id = ''.join(sorted(users))
-        self.users = users.copy()
-        self.resources = []
-        self.parents = []
-        self.children = []
-    
+        self.users = set(users.copy())
+        self.resources = set()
+        self.parents = set()
+        self.children = set()
+
     def get_users(self):
         return self.users
-    
-    def add_resources(self, resources):
-        self.resources = resources
-    
-    def print_context(self):
 
-        context_str = f"{self.id}\n" + "{" + ', '.join(self.users) +"}" + "\n" "[ " + ', '.join(self.resources) +"]"
+    def get_resources(self):
+        return self.resources
+
+    def add_resource(self, resource):
+        self.resources.add(resource)
+
+    def remove_resource(self, resource):
+        self.resources.remove(resource)
+
+    def print_context(self):
+        context_str = (f"{self.id}\n" + "{" + ', '.join(list(self.users)) + "}" +
+                       "\n" "[" + ', '.join(list(self.resources)) + "]")
         # print(context_str)
         return context_str
-        
+
 
 class Network:
 
@@ -30,16 +38,15 @@ class Network:
         self.all_users = users
         self.contexts = {}
         self.create_network()
-    
+
     def add_new_user(self, user):
         self.all_users.append(user)
-        self.update_network()
+        self.expand_network()
 
     def add_context(self, context):
 
         if context.id not in self.contexts.keys():
             self.contexts[context.id] = context
-        
 
     def create_network(self):
 
@@ -49,18 +56,13 @@ class Network:
         # Recursively generate child contexts with one less user until leaf level with two users
         self.generate_child_contexts(root_context, len(self.all_users))
 
-
-
-    def update_network(self):
+    def expand_network(self):
 
         new_root_context = Context(self.all_users)
         self.add_context(new_root_context)
 
         # Recursively generate child contexts with one less user until leaf level with two users
         self.generate_child_contexts(new_root_context, len(self.all_users))
-
-
-    
 
     def generate_child_contexts(self, parent_context, num_users):
 
@@ -69,7 +71,7 @@ class Network:
 
         # Generate child contexts with one less user
         user_combinations = [list(comb) for comb in combinations(parent_context.users, num_users - 1)]
-        
+
         for child_users in user_combinations:
 
             child_context_id = ''.join(sorted(child_users))
@@ -81,21 +83,20 @@ class Network:
                 child_context = Context(child_users)
                 self.add_context(child_context)
 
-            parent_context.children.append(child_context)
-            child_context.parents.append(parent_context)
+            parent_context.children.add(child_context)
+            child_context.parents.add(parent_context)
 
             self.generate_child_contexts(child_context, num_users - 1)
 
-
     def print_network(self):
-        
+
         root_context = self.contexts[''.join(self.all_users)]
-        
+
         queue = deque([root_context])
         visited = {}
 
         print("Root Context:")
-        root_context.print_context()
+        print(root_context.print_context())
         visited[root_context.id] = True
 
         print("\nChild Contexts:")
@@ -104,11 +105,10 @@ class Network:
             current_context = queue.popleft()
             for child_context in current_context.children:
                 if child_context not in visited.keys():
-                    child_context.print_context()
+                    print(child_context.print_context())
                     queue.append(child_context)
                     visited[child_context] = True
 
-                
     def visualize_network(self):
         G = pgv.AGraph(directed=True)
 
@@ -125,30 +125,84 @@ class Network:
                     visited[child_context] = True
 
         G.layout(prog='dot')
-        G.draw(f'collaboration_network_{root_context.id}.png')
+        G.draw(f'collaboration_network_{root_context.id}_{datetime.datetime.now()}.png')
         print("Collaboration network visualization saved as collaboration_network.png")
 
+    def share_resource(self, from_user, resource_to_share, to_users):
 
-    def update_context_resource(self, from_user, resource, to_users):
+        involved_users = to_users.union({from_user})
 
-        involved_users = to_users.append(from_user)
+        root_context = self.contexts[''.join(self.all_users)]
+        queue = deque([root_context])
+        visited = {}
+
+        already_shared_context = None
+
+        while queue:
+            current_context = queue.popleft()
+
+            resources = current_context.get_resources()
+
+            if resource_to_share in resources:
+
+                already_shared_context = current_context
+                current_context.remove_resource(resource_to_share)
+                break
+
+            else:
+                for child_context in current_context.children:
+                    if child_context not in visited.keys():
+                        queue.append(child_context)
+                        visited[child_context] = True
+
+        if already_shared_context is None:
+            correct_context = ''.join(sorted(involved_users))
+        else:
+            already_shared_users = already_shared_context.get_users()
+            correct_users = involved_users.union(already_shared_users)
+            correct_context = ''.join(sorted(correct_users))
+
+        self.contexts[correct_context].add_resource(resource_to_share)
+        print(f"Correct Context: {correct_context}")
+
+        self.visualize_network()
+
+    def unshare_resource(self, from_user, resource_to_unshare, to_users):
+
+        involved_users = to_users.union({from_user})
+
         root_context = self.contexts[''.join(self.all_users)]
         queue = deque([root_context])
         visited = {}
 
         while queue:
             current_context = queue.popleft()
-            
-            current_context_users = current_context.get_users()
 
-            if all(user in current_context_users for user in involved_users):
-                
-                other_users = list(set(current_context_users) - set(involved_users))
+            users = current_context.get_users()
+            resources = current_context.get_resources()
 
-                
-            
+            if involved_users.issubset(users):
+                if resource_to_unshare in resources:
+
+                    current_context.remove_resource(resource_to_unshare)
+
+                    additional_users = users.difference(involved_users)
+                    print(f"Additional Users: {additional_users}")
+                    # If the context to unshare is a leaf node, then nothing else to do
+                    if len(additional_users) == 0:
+                        self.visualize_network()
+                        return
+
+                    correct_users = additional_users.union({from_user})
+                    correct_context_id = ''.join(sorted(correct_users))
+
+                    for child_context in current_context.children:
+                        if child_context.id == correct_context_id:
+                            child_context.add_resource(resource_to_unshare)
+                            self.visualize_network()
+                            return
+
             for child_context in current_context.children:
                 if child_context not in visited.keys():
                     queue.append(child_context)
                     visited[child_context] = True
-
--- main.py
+++ main.py
@@ -2,24 +2,30 @@ from classes.resource import Resource
 from classes.user import User, Users
 from classes.collab import Network
 
+
 def main():
-    
     User("A")
     User("B")
     User("C")
 
     for uid, user in Users.users.items():
         for i in range(1, 5):
-            r = Resource(rid = f"{uid}{i}", owner = user)
+            r = Resource(rid=f"{uid}{i}", owner=user)
             user.addOwnerResource(r)
 
     network = Network(list(Users.users.keys()))
-    network.visualize_network()
+
+    network.share_resource("A", "A_1", {"B", "C"})
+    network.share_resource("A", "A_2", {"B", "C"})
+    network.unshare_resource("A", "A_2", {"B"})
+    network.share_resource("A", "A_3", {"B", "C"})
+    network.share_resource("A", "A_4", {"B"})
+    network.share_resource("B", "B_1", {"C"})
 
     User("D")
     network.add_new_user("D")
-    network.visualize_network()
-    
+
+    network.share_resource("A", "A_1", {"D"})
 
 
 if __name__ == "__main__":
