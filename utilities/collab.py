e0d40544361d99bf4c1ae440dd617b4e4ba5e719
Souradip Nath <anihero4@gmail.com>
2024-05-15 21:35:38 -0700

    Fixed can_unshare

--- utilities/collab.py
+++ utilities/collab.py
@@ -290,7 +290,7 @@ def can_unshare(from_username: str, resource_id: str, to_username: str, project_
                         collaborators = context.get_users()
                         if str(to_user_id) in collaborators and str(from_user_id) in collaborators:
                             resources = context.get_resources()
-                            if resource_id in resources:
+                            if resource_path in resources:
                                 print(f"Un-Sharing {resource_path} Allowed: From {from_username} to {to_username}")
                                 return True
 
