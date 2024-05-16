098d4b760a40834e9891833e718c74210b76ded1
confusedDip <anihero4@gmail.com>
2024-05-15 22:44:06 -0500

    edited share()

--- classes/collab.py
+++ classes/collab.py
@@ -226,6 +226,7 @@ class Network:
 
         for context_id, context in self.__contexts.items():
             resources = context.get_resources()
+            print(f"{context_id}: {resources}")
 
             # If resource is already shared within the current context
             # Keep a note of the context and remove the resource to elevate later
