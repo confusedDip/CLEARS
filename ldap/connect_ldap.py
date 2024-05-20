43cc83718107ed915eec8376fa3d332e8acf2c48
confusedDip <anihero4@gmail.com>
2024-05-20 02:25:03 -0700

    trying to save my boat using ldap

--- ldap/connect_ldap.py
+++ ldap/connect_ldap.py
@@ -2,7 +2,7 @@ from ldap3 import Server, Connection
 
 
 def connect_to_ldap(
-        server_url="ldap:///10.218.108.176:389",
+        server_url="ldap://10.218.108.176",
         username="cn=admin,dc=rc,dc=example,dc=org",
         password="Bally@2016"
 ):
