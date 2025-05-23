1a0aaecb37b7e63ab7cd34951c2b22967ff334e2
confusedDip <anihero4@gmail.com>
2025-05-23 13:50:16 +0530

    Debugging

--- install.sh
+++ install.sh
@@ -4,29 +4,24 @@
 set -e
 
 # --- CONFIGURATION ---
-REPO_URL="https://github.com/confusedDip/collab-network.git"  # Replace this with your actual GitHub/Repo URL
 APP_DIR="/usr/bin/authz"
 SYMLINK="/usr/bin/clears"
 
-echo "[1/6] Cloning the repository..."
-git clone "$REPO_URL"
-REPO_NAME=$(basename "$REPO_URL" .git)
-cd "$REPO_NAME"
 
-echo "[2/6] Installing Python dependencies..."
+echo "[1/5] Installing Python dependencies..."
 pip3 install -r requirements.txt
 
-echo "[3/6] Setting up deployment directory at $APP_DIR..."
+echo "[2/5] Setting up deployment directory at $APP_DIR..."
 sudo mkdir -p "$APP_DIR"
 
-echo "[4/6] Copying files to $APP_DIR..."
+echo "[3/5] Copying files to $APP_DIR..."
 sudo cp -r * "$APP_DIR"
 
-echo "[5/6] Updating permissions for privileged wrappers..."
+echo "[4/5] Updating permissions for privileged wrappers..."
 sudo chmod +s "$APP_DIR/utilities/wrapper_network_dump"
 sudo chmod +s "$APP_DIR/utilities/wrapper_supdate"
 
-echo "[6/6] Creating symbolic link at $SYMLINK..."
+echo "[5/5] Creating symbolic link at $SYMLINK..."
 sudo ln -sf "$APP_DIR/main.py" "$SYMLINK"
 sudo chmod +x "$SYMLINK"
 
